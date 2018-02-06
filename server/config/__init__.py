2f83c8a68e92a9380e25f00ab210e3115db89713
Bernard Ojengwa <bernardojengwa@gmail.com>
2018-02-06 17:59:40 +0100

    Update server/config/__init__.py
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- server/config/__init__.py
+++ server/config/__init__.py
@@ -1,10 +1,10 @@
 from .test import TestConfig
 from .production import ProdConfig
-from .heroku import HerokuConfig
+from .staging import StagingConfig
 
 try:
     from .development import DevConfig
 except Exception as e:
     DevConfig = None
 
-__all__ = ['DevConfig', 'TestConfig', 'ProdConfig', 'HerokuConfig']
+__all__ = ['DevConfig', 'TestConfig', 'ProdConfig', 'StagingConfig']
