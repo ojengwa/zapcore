1d8605471789faf780e37736e64130a07e8c65c8
Bernard Ojengwa <bernardojengwa@gmail.com>
2018-02-06 19:51:27 +0100

    Update server/config/test.py
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- server/config/test.py
+++ server/config/test.py
@@ -5,5 +5,8 @@ class TestConfig(Config):
     ''' Configuration class for site testing environment '''
 
     SQLALCHEMY_DATABASE_URI = 'sqlite://'
-    TEST = True
+    TESTING = True
     SQLALCHEMY_ECHO = True
+
+    LIVESERVER_PORT = 0
+    LIVESERVER_TIMEOUT = 10
