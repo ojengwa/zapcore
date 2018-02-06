9fff904c11a2e7e036556f1ae7a9eee00b124ac3
Bernard Ojengwa <bernardojengwa@gmail.com>
2018-02-06 23:42:10 +0100

    Update fabfile.py
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*- ##
 
 import uuid
+
+from contextlib import contextmanager
 from fabric.api import env, prompt, local
 
 
@@ -8,8 +10,6 @@ def source_settings():
     env.SOURCE_PATH = '/zapcore'
     env.colorize_errors = True
     env.git_path = 'git@github.com:ojengwa/zapcore.git'
-    env.AWS_ACCESS_KEY = 'AKIAIFUHSUNLQERBEPYQ'
-    env.AWS_ACCESS_SECRET = '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'
 
 
 def ifnotsetted(key, default, is_prompt=False, text=None, validate=None):
@@ -29,6 +29,20 @@ def common_settings():
     source_settings()
 
 
+def prod_settings():
+    common_settings()
+
+
+@contextmanager
+def zappa_env():
+    orig_shell = env['shell']
+    env_vars_str = ' '.join('{0}={1}'.format(key, value)
+                            for key, value in env.items())
+    env['shell'] = '{0} {1}'.format(env_vars_str, orig_shell)
+    yield
+    env['shell'] = orig_shell
+
+
 def dev_server():
     local('python manage.py runserver')
 
@@ -42,73 +56,87 @@ def assets():
 
 
 def init():
-    source_settings()
-    local('zappa init')
+    prod_settings()
+    with zappa_env():
+        local('zappa init')
 
 
-def deploy(environment=''):
-    source_settings()
-    local('zappa deploy {0}'.format(environment))
+def deploy(environment):
+    prod_settings()
+    local('printenv')
+    with zappa_env():
+        local('zappa deploy {0}'.format(environment))
 
 
-def undeploy(environment='', remove_logs=False):
+def undeploy(environment, remove_logs=False):
     source_settings()
     if remove_logs:
         cmd = 'zappa undeploy {0} --remove-logs'.format(environment)
     else:
         cmd = 'zappa undeploy {0}'.format(environment)
 
-    local(cmd)
+    with zappa_env():
+        local(cmd)
 
 
-def update(environment=''):
-    source_settings()
-    local('zappa update {0}'.format(environment))
+def update(environment):
+    prod_settings()
+    with zappa_env():
+        local('zappa update {0}'.format(environment))
 
 
-def rollback(environment='', revisions=1):
-    source_settings()
-    local('zappa rollback {0} -n {1}'.format(environment, revisions))
+def rollback(environment, revisions=1):
+    prod_settings()
+    with zappa_env():
+        local('zappa rollback {0} -n {1}'.format(environment, revisions))
 
 
-def schedule(environment=''):
-    source_settings()
-    local('zappa schedule {0}'.format(environment))
+def schedule(environment):
+    prod_settings()
+    with zappa_env():
+        local('zappa schedule {0}'.format(environment))
 
 
-def unschedule(environment=''):
-    source_settings()
-    local('zappa unschedule {0}'.format(environment))
+def unschedule(environment):
+    prod_settings()
+    with zappa_env():
+        local('zappa unschedule {0}'.format(environment))
 
 
-def pack(environment='', storage_path='{0}.zip'.format(uuid.uuid4())):
-    source_settings()
-    local('zappa package {0} -o {1}'.format(environment, storage_path))
+def pack(environment, storage_path='{0}.zip'.format(uuid.uuid4())):
+    prod_settings()
+    with zappa_env():
+        local('zappa package {0} -o {1}'.format(environment, storage_path))
 
 
-def cloudformation(lambda_arn, role_arn, environment=''):
-    source_settings()
-    local(
-        'zappa template {0} --l {1} -r {2}'.format(
-            lambda_arn, role_arn, environment))
+def cloudformation(lambda_arn, role_arn, environment):
+    prod_settings()
+    with zappa_env():
+        local(
+            'zappa template {0} --l {1} -r {2}'.format(
+                lambda_arn, role_arn, environment))
 
 
-def status(environment=''):
-    source_settings()
-    local('zappa status {0}'.format(environment))
+def status(environment):
+    prod_settings()
+    with zappa_env():
+        local('zappa status {0}'.format(environment))
 
 
-def log(environment='', events='', since=''):
-    source_settings()
-    local('zappa tail {0} --{1} --since'.format(environment, events, since))
+def log(environment, events='', since=''):
+    prod_settings()
+    with zappa_env():
+        local(
+            'zappa tail {0} --{1} --since'.format(environment, events, since))
 
 
-def invoke(cmd, environment='', verbose=True):
-    source_settings()
+def invoke(environment, cmd, verbose=True):
+    prod_settings()
 
     if verbose:
         text = 'zappa invoke {0} --{1} --raw'.format(environment, cmd)
     else:
         text = 'zappa invoke {0} --{1}'.format(environment, cmd)
 
-    local(text)
+    with zappa_env():
+        local(text)
