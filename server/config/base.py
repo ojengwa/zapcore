2de9b2ee3a58aacd2055e58771069b2e178e5433
Bernard Ojengwa <bernardojengwa@gmail.com>
2018-02-06 17:59:40 +0100

    Update server/config/base.py
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- server/config/base.py
+++ server/config/base.py
@@ -1,6 +1,8 @@
 import os
+from logging import getLogger
 
-from urllib.parse import urlparse
+
+logger = getLogger(__name__)
 
 
 class Config(object):
@@ -12,57 +14,18 @@ class Config(object):
 
     DEBUG = True
     FLASK_DEBUG = 1
+
     SECRET_KEY = os.getenv(
         'SECRET_KEY',
         '\x91c~\xc0-\xe3\'f\xe19PE\x91`6\x01/\x0c\xed\\\xbdk\xf8')
 
     SQLALCHEMY_ECHO = False
-    # Mail settings
-    MAIL_SERVER = 'smtp.mailgun.org'
-    MAIL_PORT = 587
-    MAIL_USERNAME = 'postmaster@myinform.mailgun.org'
-    MAIL_PASSWORD = 'myinform'
-    DEFAULT_MAIL_CHANNEL = 'mailgun'
-    MAIL_DEFAULT_SENDER = 'daemon@inform.com'
-    MAIL_SUPPRESS_SEND = True
-    MAIL_FAIL_SILENTLY = False
-    UPLOADS_DEFAULT_DEST = os.path.join(
-        os.path.dirname(os.path.abspath(__name__)), 'uploads')
-    CLOUDFILES_USERNAME = 'inform'
-    CLOUDFILES_API_KEY = '80a129ef023b8027f4ba4523786feacf'
-
+    DATABASE_URL = os.getenv('DATABASE_URL')
     PROTOCOL = 'http://'
-
-    ADMIN_USERNAME = 'inform'
-    ADMIN_PASSWORD = 'inform'
-    ADMIN_EMAIL = 'admin@verifi.ng'
-    ADMIN_FULL_NAME = 'Inform Admin'
 
     ASSETS_DEBUG = False  # not DEBUG
 
-    NAMEKO_AMQP_URI = os.getenv('NAMEKO_AMQP_URI',
-                                'amqp://localhost')
-    NAMEKO_INITIAL_CONNECTIONS = 3
-    NAMEKO_MAX_CONNECTIONS = 10
-
-    REDIS_URL = os.getenv(
-        'REDIS_URL',
-        'redis://redistogo:f8a84dd02ddd32d65d9ae643a8f25543'
-        '@jack.redistogo.com:11455/')
-
-    if REDIS_URL:
-        url = urlparse(REDIS_URL)
-        REDIS_HOST = url.hostname
-        REDIS_PORT = url.port
-        REDIS_PASSWORD = url.password
-
-    CELERY_TIMEZONE = 'Africa/Lagos'
-
-    LOGFILE_NAME = 'inform'
-
-    LOGIN_VIEW = 'auth.login'
-
-    # DOMAIN = 'inform.dev:2500'
+    LOGFILE_NAME = 'zapcore'
 
     AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY', 'AKIAI7KGQ4FVKQ5EV3LA')
 
@@ -80,13 +43,5 @@ class Config(object):
     }
     USE_S3_DEBUG = not USE_S3
 
-    SETUP_DIR = os.path.join(os.path.dirname(
-        os.path.abspath(__name__)), 'setup/')
-
     BASE_DIR = os.path.dirname(
         os.path.dirname(os.path.abspath(__file__)))
-
-    TEMPLATE_DIR = os.path.join(os.path.dirname(
-        os.path.abspath(__name__)), 'templates/')
-    STATIC_DIR = os.path.join(os.path.dirname(
-        os.path.abspath(__name__)), 'static/')
