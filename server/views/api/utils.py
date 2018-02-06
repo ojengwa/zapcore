4bb715a23bc24d2160b9b0770ea8b36834a3e8eb
Bernard Ojengwa <bernardojengwa@gmail.com>
2018-02-06 17:34:08 +0100

    Update server/views/api/utils.py
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- server/views/api/utils.py
+++ server/views/api/utils.py
@@ -1,52 +1,5 @@
 import arrow
 
-from datetime import date
-
-
-def clean(collection):
-    del collection['_id']
-
-    return collection
-
-
-def _search(bvn, app):
-    bio = app.db.bios.find_one(
-        {'_id': bvn}, projection=['bvn', 'first_name', 'last_name',
-                                  'gender', 'dob', 'created_at'])
-
-    if not bio:
-        bio = {'status': 'error', 'data': None,
-               'message': 'Unknown Bank Verification Number'}
-
-    bio['dob'] = get_age(bio.get('dob'))
-    bio['created_at'] = humanize_date(bio.get('created_at'))
-
-    payrolls = app.db.payrolls.find({'_id': bvn})
-    work = app.db.work_histories.find({'_id': bvn})
-    data = {}
-    data['payrolls'] = payrolls.distinct('rows')
-    data['work_histories'] = work.distinct('rows')
-    data['mortgages'] = []
-    data['rents'] = []
-    data['utilities'] = []
-    data['loans'] = []
-
-    bio.update(buckets=data)
-
-    return bio
-
-
-def get_age(born=None):
-
-    if not born:
-        return 'No Age Record'
-    born = arrow.get(born).datetime
-    today = date.today()
-    age = today.year - born.year - (
-        (today.month, today.day) < (born.month, born.day))
-
-    return '{0} years'.format(age)
-
 
 def humanize_date(value):
     return arrow.get(value).humanize()
