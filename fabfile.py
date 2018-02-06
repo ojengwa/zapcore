2614b9b7db27adb2c631ce2cddf71e8cb96b5837
Bernard Ojengwa <bernardojengwa@gmail.com>
2018-02-06 23:19:16 +0100

    Update fabfile.py
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- fabfile.py
+++ fabfile.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*- ##
 
-import os
-from fabric.api import env, run, sudo, require, prompt, put, local
+import uuid
+from fabric.api import env, run as _run, sudo, require, prompt, put, local
 
 
 def source_settings():
-    env.SOURCE_PATH = 'src/zapcore'
+    env.SOURCE_PATH = '/zapcore'
+    env.colorize_errors = True
     env.git_path = 'git@github.com:ojengwa/zapcore.git'
+    env.AWS_ACCESS_KEY = 'AKIAIFUHSUNLQERBEPYQ'
+    env.AWS_ACCESS_SECRET = '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'
 
 
 def ifnotsetted(key, default, is_prompt=False, text=None, validate=None):
@@ -26,49 +29,81 @@ def common_settings():
     source_settings()
 
 
-def stage_settings():
-    common_settings()
+def dev_server():
+    local('python manage.py runserver')
 
 
-def production_settings():
-    common_settings()
+def test():
+    local('python manage.py test')
 
 
-def dev_setup():
-    local('pythoN')
+def assets():
+    local('python manage.py collectstatic')
 
 
-def project_setup():
-    pass
+def deploy(environment=''):
+    source_settings()
+    local('zappa deploy {0}'.format(environment))
 
 
-def setup_stage():
-    stage_settings()
+def undeploy(environment='', remove_logs=False):
+    source_settings()
+    if remove_logs:
+        cmd = 'zappa undeploy {0} --remove-logs'.format(environment)
+    else:
+        cmd = 'zappa undeploy {0}'.format(environment)
+
+    local(cmd)
 
 
-def setup_production():
-    production_settings()
+def update(environment=''):
+    source_settings()
+    local('zappa update {0}'.format(environment))
 
 
-def update_stage():
-    stage_settings()
+def rollback(environment='', revisions=1):
+    source_settings()
+    local('zappa rollback {0} -n {1}'.format(environment, revisions))
 
 
-def update_production():
-    production_settings()
+def schedule(environment=''):
+    source_settings()
+    local('zappa schedule {0}'.format(environment))
 
 
-def rollback_stage():
-    stage_settings()
+def unschedule(environment=''):
+    source_settings()
+    local('zappa unschedule {0}'.format(environment))
 
 
-def rollback_production():
-    production_settings()
+def pack(environment='', storage_path='{0}.zip'.format(uuid.uuid4())):
+    source_settings()
+    local('zappa package {0} -o {1}'.format(environment, storage_path))
 
 
-def release_stage():
-    stage_settings()
+def cloudformation(lambda_arn, role_arn, environment=''):
+    source_settings()
+    local(
+        'zappa template {0} --l {1} -r {2}'.format(
+            lambda_arn, role_arn, environment))
 
 
-def release_production():
-    production_settings()
+def status(environment=''):
+    source_settings()
+    local('zappa status {0}'.format(environment))
+
+
+def log(environment='', events='', since=''):
+    source_settings()
+    local('zappa tail {0} --{1} --since'.format(environment, events, since))
+
+
+def invoke(cmd, environment='', verbose=True):
+    source_settings()
+
+    if verbose:
+        text = 'zappa invoke {0} --{1} --raw'.format(environment, cmd)
+    else:
+        text = 'zappa invoke {0} --{1}'.format(environment, cmd)
+
+    local(text)
