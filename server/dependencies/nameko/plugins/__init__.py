fdde5d631e9086fb6ef7e3bc76532e98203c6112
Bernard Ojengwa <bernardojengwa@gmail.com>
2018-02-06 17:34:01 +0100

    Update server/dependencies/nameko/plugins/__init__.py
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- server/dependencies/nameko/plugins/__init__.py
+++
@@ -1,9 +0,0 @@
-from .base import (
-    HealthcheckPlugin, ManagementPlugin, plugin_registry, register_plugin,
-    StatsPlugin
-)  # noqa: unused variables
-from .entrypoints import ListEntrypoints  # noqa: unused variable
-
-
-__all__ = ['HealthcheckPlugin', 'ManagementPlugin', 'plugin_registry',
-           'register_plugin', 'StatsPlugin', 'ListEntrypoints']
