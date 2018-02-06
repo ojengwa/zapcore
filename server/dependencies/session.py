298d81ef84afa4a37e0d12dcc9c5551147699aad
Bernard Ojengwa <bernardojengwa@gmail.com>
2018-02-06 13:27:31 +0100

    Update server/dependencies/session.py
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

---
+++ server/dependencies/session.py
@@ -0,0 +1,67 @@
+# Database session provider
+
+
+from weakref import WeakKeyDictionary
+
+from nameko.extensions import DependencyProvider
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DB_URIS_KEY = 'DB_URIS'
+
+
+class DatabaseSession(DependencyProvider):
+    def __init__(self, declarative_base):
+        self.declarative_base = declarative_base
+        self.sessions = WeakKeyDictionary()
+
+    def setup(self):
+        service_name = self.container.service_name
+        decl_base_name = self.declarative_base.__name__
+        uri_key = '{}:{}'.format(service_name, decl_base_name)
+
+        db_uris = self.container.config[DB_URIS_KEY]
+        self.db_uri = db_uris[uri_key].format({
+            'service_name': service_name,
+            'declarative_base_name': decl_base_name,
+        })
+        self.engine = create_engine(self.db_uri)
+
+    def stop(self):
+        self.engine.dispose()
+        del self.engine
+
+    def get_dependency(self, worker_ctx):
+
+        session_cls = sessionmaker(bind=self.engine)
+        session = session_cls()
+
+        self.sessions[worker_ctx] = session
+        return session
+
+    def worker_teardown(self, worker_ctx):
+        session = self.sessions.pop(worker_ctx)
+        session.close()
+
+
+# backwards compat
+Session = DatabaseSession
+
+
+# from nameko_sqlalchemy import Session
+
+# from .models import Model, DeclarativeBase
+
+# class Service(object):
+
+#     session = Session(DeclarativeBase)
+
+#     @entrypoint
+#     def write_to_db(self):
+#         model = Model(...)
+#         self.session.add(model)
+#         self.session.commit()
+
+#     @entrypoint
+#     def query_db(self):
+#         queryset = self.session.query(Model).filter(...)
