e914ffd4635a4f52be63b2a7916a1511852fce72
Bernard Ojengwa <bernardojengwa@gmail.com>
2018-02-06 19:51:26 +0100

    Update fabfile.py
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- fabfile.py
+++ fabfile.py
@@ -1,20 +1,12 @@
 # -*- coding: utf-8 -*- ##
 
 import os
-from fabric.api import env, run, sudo, require, prompt, put
+from fabric.api import env, run, sudo, require, prompt, put, local
 
 
 def source_settings():
     env.SOURCE_PATH = 'src/zapcore'
     env.git_path = 'git@github.com:ojengwa/zapcore.git'
-
-
-def render_put(template_name, dest, params, mode=None, tempfile='tempfile'):
-    pass
-    # data = loader.render_to_string(template_name, dictionary=params)
-    # open(tempfile, 'w').write(data)
-    # put(tempfile, dest, mode)
-    # os.remove(tempfile)
 
 
 def ifnotsetted(key, default, is_prompt=False, text=None, validate=None):
@@ -27,20 +19,7 @@ def ifnotsetted(key, default, is_prompt=False, text=None, validate=None):
 
 def prompts():
     source_settings()
-    ifnotsetted('host_string', 'truckica.com', True,
-                "No hosts found. Please specify "
-                "single host string for connection")
-    ifnotsetted('user', 'root', True, "Server user name")
-    ifnotsetted('VPS_IP', '97.107.129.224', True, "VPS IP")
-    ifnotsetted('POSTGRES_USER', 'truckica', True, "PostgreSQL user name")
-    ifnotsetted('POSTGRES_PASSWORD', 'truckicaS3n89mkk',
-                True, "PostgreSQL user's password")
-    ifnotsetted('POSTGRES_DB', 'truckica', True, "PostgreSQL DATABASE")
-    ifnotsetted('UBUNTU_VERSION', 'karmic', True, "Ubuntu version name")
-    ifnotsetted('PAYPAL_EMAIL',
-                'admin_1255085897_biz@crowdsense.com', True, "PAYPAL EMAIL")
-    ifnotsetted('PAYPAL_TEST', 'True', True,
-                "PAYPAL TEST (True or False)?", r'^(True|False)$')
+    ifnotsetted('DATABASE_URL', '', True, "Database URL")
 
 
 def common_settings():
@@ -55,8 +34,8 @@ def production_settings():
     common_settings()
 
 
-def server_setup():
-    pass
+def dev_setup():
+    local('pythoN')
 
 
 def project_setup():
