da128832108b830f4ee6681c894abce481fb180d
Bernard Ojengwa <bernardojengwa@gmail.com>
2018-02-06 17:34:09 +0100

    Update server/views/main/__init__.py
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- server/views/main/__init__.py
+++ server/views/main/__init__.py
@@ -1,19 +1,8 @@
-from flask import Blueprint, current_app
+from flask import Blueprint
 
 main = Blueprint(
     'main',
     __name__
 )
 
-
-from . import errors
 from . import views
-from ...models import Permission
-
-
-# from ..models import Permission
-
-
-@main.app_context_processor
-def inject_permissions():
-    return dict(permission=Permission)
