7deab5639117c35d3afe0e213df0a01c56898e80
Bernard Ojengwa <bernardojengwa@gmail.com>
2018-02-08 03:43:50 +0100

    Update server/views/main/__init__.py
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- server/views/main/__init__.py
+++ server/views/main/__init__.py
@@ -1,10 +1,14 @@
 from __future__ import absolute_import
 
 from flask import Blueprint
+from flask_cors import CORS
 
 main = Blueprint(
     'main',
     __name__
 )
+
+# add CORS support
+CORS(main)
 
 from . import views
