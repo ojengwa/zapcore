d8ff40fee47b1713f09c6fa0edda245a218cfe98
Bernard Ojengwa <bernardojengwa@gmail.com>
2018-02-08 19:45:34 +0100

    Update server/models/__init__.py
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- server/models/__init__.py
+++ server/models/__init__.py
@@ -10,18 +10,14 @@ class BaseModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     created_on = db.Column(db.DateTime, nullable=False, default=now)
 
-    def __init__(self, classtype, *args, **kwargs):
-        super(BaseModel, self).__init__(*args, **kwargs)
-        self._type = classtype
 
-
-def model_factory(name, argnames, BaseModel=BaseModel):
+def class_factory(name, argnames, BaseClass=BaseModel):
     def __init__(self, **kwargs):
         for key, value in kwargs.items():
             if key not in argnames:
                 raise TypeError("Argument %s not valid for %s"
                                 % (key, self.__class__.__name__))
             setattr(self, key, value)
-        BaseModel.__init__(self, name[:-len("Class")])
-    newclass = type(name, (BaseModel,), {"__init__": __init__})
+        BaseClass.__init__(self, name[:-len("Class")])
+    newclass = type(name, (BaseClass,), {"__init__": __init__})
     return newclass
