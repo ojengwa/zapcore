7b26d3f03c5ce4f9782650acc7ef7dacd44bbe45
Bernard Ojengwa <bernardojengwa@gmail.com>
2018-02-08 03:06:35 +0100

    Update server/utils/create.py
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

---
+++ server/utils/create.py
@@ -0,0 +1,15 @@
+class BaseClass(object):
+    def __init__(self, classtype):
+        self._type = classtype
+
+
+def ClassFactory(name, argnames, BaseClass=BaseClass):
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            if key not in argnames:
+                raise TypeError("Argument %s not valid for %s"
+                                % (key, self.__class__.__name__))
+            setattr(self, key, value)
+        BaseClass.__init__(self, name[:-len("Class")])
+    newclass = type(name, (BaseClass,), {"__init__": __init__})
+    return newclass
