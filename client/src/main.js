7129b6fe73d41ed82e4f8e7f8eb10c0f945d6939
Bernard Ojengwa <bernardojengwa@gmail.com>
2018-02-09 15:16:48 +0100

    Update client/src/main.js
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,11 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import axios from 'axios';
 import Vue from 'vue';
 import App from './App';
 import router from './router';
+
+const baseApiPath = '/_api';
 
 Vue.config.productionTip = false;
 
@@ -10,6 +13,8 @@ Vue.config.productionTip = false;
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {
+    App,
+  },
   template: '<App/>',
 });
